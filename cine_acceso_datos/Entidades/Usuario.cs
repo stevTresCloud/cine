d6a543ec6e795163fd3f19cf5702ee1116f99a0e
Steven Luna <78817305+stevTresCloud@users.noreply.github.com>
2024-01-20 15:28:10 -0500

    [ADD] extrem_cinema: Proyecto en clases - División de modelo de proyecto - Creación inicial de clases y proyectos

---
+++ cine_acceso_datos/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    public class Usuario
+    {
+        private int id { get; set; }
+        private string nombre { get; set; }
+        private string clave { get; set; }
+        private string correo { get; set; }
+        private string tipo { get; set; }
+    }
+}
---
+++ cine_logica_negocio/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_logica_negocio
+{
+    public class Class1
+    {
+
+    }
+}
